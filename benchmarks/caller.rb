f000544eab35cce038997e9e4b61307cbc22419b
HeRoMo <hero@asterisk-works.jp>
2019-07-14 18:31:40 +0900

    add caller

---
+++ benchmarks/caller.rb
@@ -0,0 +1,35 @@
+require 'benchmark_driver'
+
+output = :gruff
+versions = ['2.3.8', '2.4.6', '2.5.4', '2.6.3', '2.7.0-preview1']
+
+Benchmark.driver(output: output) do |x|
+  x.rbenv *versions
+
+  x.prelude <<~RUBY
+    def bad_sample1
+      caller[1]
+    end
+
+    def bad_sample2
+      caller.first
+    end
+    def bad_sample3
+      caller_locations.first
+    end
+
+    def good_sample1
+      caller(1..1).first
+    end
+
+    def good_sample2
+      caller_locations(1..1).first
+    end
+  RUBY
+
+  x.report %{ bad_sample1 }
+  x.report %{ bad_sample2 }
+  x.report %{ bad_sample3 }
+  x.report %{ good_sample1 }
+  x.report %{ good_sample2 }
+end
