b2135bc5560a22dce7df86927f9899e85eed71d0
HeRoMo <hero@asterisk-works.jp>
2019-07-15 15:56:00 +0900

    add UriDefaultParser

---
+++ benchmarks/uri-default-parser.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# rubocop-performance
+# Performance/UriDefaultParser
+# see https://github.com/rubocop-hq/rubocop-performance/blob/master/manual/cops_performance.md#performanceuridefaultparser
+
+require 'benchmark_driver'
+
+output = :gruff
+versions = ['2.3.8', '2.4.6', '2.5.4', '2.6.3', '2.7.0-preview1']
+
+Benchmark.driver(output: output, runner: :time) do |x|
+  x.rbenv *versions
+
+  x.prelude <<~RUBY
+    URL_STR = 'https://www.sample.com'
+
+    def bad_sample
+      URI::Parser.new.parse(URL_STR)
+    end
+
+    def good_sample
+      URI::DEFAULT_PARSER.parse(URL_STR)
+    end
+
+    def exstra_sample
+      URI.parse(URL_STR)
+    end
+  RUBY
+
+  x.report %{ bad_sample }
+  x.report %{ good_sample }
+  x.report %{ exstra_sample }
+end
