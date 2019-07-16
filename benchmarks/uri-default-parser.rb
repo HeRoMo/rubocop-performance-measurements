f3d01f8d6ea773cea55fa3cccb3e965f3ac976fd
HeRoMo <hero@asterisk-works.jp>
2019-07-16 23:41:10 +0900

    fix bug

--- benchmarks/uri-default-parser.rb
+++ benchmarks/uri-default-parser.rb
@@ -9,7 +9,7 @@ require 'benchmark_driver'
 output = :gruff
 versions = ['2.3.8', '2.4.6', '2.5.4', '2.6.3', '2.7.0-preview1']
 
-Benchmark.driver(output: output, runner: :time) do |x|
+Benchmark.driver(output: output) do |x|
   x.rbenv *versions
 
   x.prelude <<~RUBY
