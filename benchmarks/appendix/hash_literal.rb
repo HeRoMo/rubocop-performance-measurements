46a1ac3876e8b8afb3e9efdd70d54eb1146f5132
HeRoMo <hero@asterisk-works.jp>
2019-12-27 00:53:36 +0900

    update to ruby 2.7

--- benchmarks/appendix/hash_literal.rb
+++ benchmarks/appendix/hash_literal.rb
@@ -3,7 +3,7 @@
 require 'benchmark_driver'
 
 output = :gruff
-versions = ['2.3.8', '2.4.6', '2.5.4', '2.6.3', '2.7.0-preview1']
+versions = ['2.3.8', '2.4.9', '2.5.7', '2.6.5', '2.7.0']
 
 Benchmark.driver(output: output) do |x|
   x.rbenv *versions
